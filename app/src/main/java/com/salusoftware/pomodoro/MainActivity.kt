ec6ed74e0d89f0cc6360f77a48be8b29d7c23146
Renan Salustiano <renansalustiano2020@gmail.com>
2023-03-18 20:46:44 -0300

    Implementação do viewBinding

--- app/src/main/java/com/salusoftware/pomodoro/MainActivity.kt
+++ app/src/main/java/com/salusoftware/pomodoro/MainActivity.kt
@@ -2,10 +2,16 @@ package com.salusoftware.pomodoro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.salusoftware.pomodoro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root;
+
+        setContentView(view)
     }
 }
