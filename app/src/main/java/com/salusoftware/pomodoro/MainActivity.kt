78c8115e8222d390d6cc42f99d0a2d7b35e46990
Renan Salustiano <renansalustiano2020@gmail.com>
2023-03-19 00:35:03 -0300

    Criação da lógica do timer + adição de ações aos botões: Play/Pause, Reset

--- app/src/main/java/com/salusoftware/pomodoro/MainActivity.kt
+++ app/src/main/java/com/salusoftware/pomodoro/MainActivity.kt
@@ -2,10 +2,16 @@ package com.salusoftware.pomodoro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import com.salusoftware.pomodoro.databinding.ActivityMainBinding
+import com.salusoftware.pomodoro.models.Config
+import java.util.Calendar
 
-class MainActivity : AppCompatActivity() {
+class MainActivity() : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    lateinit var config: Config
+    lateinit var timer: CountDownTimer
+    var timerLeft: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,6 +19,83 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root;
         setContentView(view)
 
-        this.binding.txtTime.text = "00:00"
+        this.config = Config()
+        this.timer = this.createTimer(this.config.workTime)
+
+        setActions()
+        this.setTimeText(this.config.workTime)
     }
+
+
+    private fun setActions(){
+
+        var state = "not_started"
+
+        this.binding.btnPlay.setOnClickListener{
+
+            state  = when(state){
+                "not_started" -> {
+                    this.timer.start()
+                    this.binding.btnPlay.setImageResource(R.drawable.ic_btn_pause)
+                    "started"
+                }
+                "started" -> {
+                    this.timer.cancel()
+                    this.binding.btnPlay.setImageResource(R.drawable.ic_btn_play)
+                    "paused"
+                }
+                "paused" -> {
+                    this.timer = createTimer(this.timerLeft)
+                    this.timer.start()
+
+                    this.binding.btnPlay.setImageResource(R.drawable.ic_btn_pause)
+                    "started"
+                }
+                else -> ""
+            }
+        }
+
+        this.binding.btnReset.setOnClickListener{
+            this.timer.cancel()
+            this.timerLeft = this.config.workTime
+
+            this.binding.btnPlay.setImageResource(R.drawable.ic_btn_pause)
+            state = "started"
+
+            this.timer = createTimer(this.config.workTime)
+            this.timer.start()
+        }
+
+        this.binding.btnConfig.setOnClickListener{
+
+        }
+    }
+
+    private fun createTimer(time: Long): CountDownTimer {
+        val context : MainActivity = this
+        return object: CountDownTimer(time, 1){
+            override fun onTick(timeInMillis: Long) {
+                context.timerLeft = timeInMillis
+
+                context.setTimeText(timeInMillis)
+            }
+
+            override fun onFinish() {
+               context.setTimeText(context.config.workTime)
+            }
+        }
+    }
+
+    fun setTimeText(time: Long){
+        val c = Calendar.getInstance()
+        c.timeInMillis = time
+        val seconds = c.get(Calendar.SECOND).toString().padStart(2,'0')
+        val minutes = c.get(Calendar.MINUTE).toString().padStart(2, '0')
+
+        val display  = "${minutes}:${seconds}"
+
+        this.binding.txtTime.text = display
+    }
+
+
 }
