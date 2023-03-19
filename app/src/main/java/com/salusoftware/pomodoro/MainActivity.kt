efe8ac3600630d98a397e5861f4115d7560b541d
Renan Salustiano <renansalustiano2020@gmail.com>
2023-03-19 15:38:37 -0300

    Melhorias no código + implementação da notificação ao finalizar timer

--- app/src/main/java/com/salusoftware/pomodoro/MainActivity.kt
+++ app/src/main/java/com/salusoftware/pomodoro/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.salusoftware.pomodoro
+import android.Manifest
+import android.content.pm.PackageManager
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.View
 import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.salusoftware.pomodoro.databinding.ActivityMainBinding
 import com.salusoftware.pomodoro.databinding.DialogConfigBinding
 import com.salusoftware.pomodoro.models.Config
-import java.util.Calendar
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity() : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -18,6 +24,7 @@ class MainActivity() : AppCompatActivity() {
     var timerLeft: Long = 0
     var currentFlow: String = "work"
     var state : String =  "not_started"
+    val notification_channel = "channel1"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,10 +48,15 @@ class MainActivity() : AppCompatActivity() {
     }
 
 
+    /**
+     * Adiciona as ações  aos botões
+     */
     private fun setActions(){
+
         this.binding.btnPlay.setOnClickListener{
             this.state  = when(this.state){
-                "not_started" -> {
+                "not_started"
+                -> {
                     this.binding.btnConfig.visibility = View.GONE
                     this.binding.labelBtnConfig.visibility = View.GONE
                     this.binding.switchMode.visibility = View.GONE
@@ -110,6 +122,9 @@ class MainActivity() : AppCompatActivity() {
         }
     }
 
+    /**
+     * Cria um novo countdown
+     */
     private fun createTimer(time: Long): CountDownTimer {
         val context : MainActivity = this
         return object: CountDownTimer(time, 1){
@@ -120,37 +135,43 @@ class MainActivity() : AppCompatActivity() {
 
             override fun onFinish() {
                 context.state = "not_started"
+                var message: String = "";
 
-                if(currentFlow == "work"){
-                    currentFlow = "rest"
-                    context.setTimeText(context.config.restTime)
-                    context.binding.labelFlow.text = getString(R.string.tempo_de_descanso)
-                }else{
-                    currentFlow = "work"
-                    context.setTimeText(context.config.workTime)
-                    context.binding.labelFlow.text = getString(R.string.tempo_de_trabalho)
+                context.currentFlow = when(context.currentFlow){
+                    "work" -> {
+                        context.setTimeText(context.config.restTime)
+                        context.binding.labelFlow.text = getString(R.string.tempo_de_descanso)
+                        message = "Tempo de trabalho finalizado!"
+                        "rest"
+                    }
+                    else -> {
+                        context.setTimeText(context.config.workTime)
+                        context.binding.labelFlow.text = getString(R.string.tempo_de_trabalho)
+                        message = "Tempo de descanso finalizado!"
+                        "work"
+                    }
                 }
 
                 context.binding.btnPlay.setImageResource(R.drawable.ic_btn_play)
-
-
-                val message = if (context.currentFlow == "work")   "Tempo de descanso finalizado!" else  "Tempo de trabalho finalizado!"
 
                 AlertDialog.Builder(context)
                     .setTitle(R.string.btn_config_text)
                     .setTitle("Tempo finalizado")
                     .setMessage(message)
-                    .setPositiveButton("Ok") { _, _ ->
-                      context.stopSound()
-                    }.setOnDismissListener {
+                    .setPositiveButton("Ok") { _, _ -> }
+                    .setOnDismissListener {
                         context.stopSound()
                     }.show()
 
-               context.playSound()
+                context.notification("Tempo finalizado", message)
+                context.playSound()
             }
         }
     }
 
+    /**
+     * Adiciona o texto na viewText de acordo com os milisegundos informados
+     */
     fun setTimeText(time: Long){
         val c = Calendar.getInstance()
         c.timeInMillis = time
@@ -193,16 +214,46 @@ class MainActivity() : AppCompatActivity() {
     }
 
     private fun playSound(){
-        if(!::mp.isInitialized){
-            mp = MediaPlayer.create(this@MainActivity, R.raw.alarm)
-            mp!!.isLooping = true
-            mp!!.start()
-        }else mp!!.start()
+        this.mp = MediaPlayer.create(this@MainActivity, R.raw.alarm)
+        this.mp!!.start()
     }
 
     private fun stopSound(){
         if(::mp.isInitialized)
-            mp!!.stop()
+            this.mp!!.stop()
+            this.mp!!.release()
+    }
+
+    /**
+     * Envia notificação
+     */
+    private fun notification(title: String, message: String){
+        val date = Date()
+        val notificationId = SimpleDateFormat("ddHHmmss", Locale.US).format(date).toInt()
+
+        val builder = NotificationCompat.Builder(this, this.notification_channel)
+            .setSmallIcon(R.drawable.ic_btn_play)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        val notificationManager  = NotificationManagerCompat.from(this)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+
+        notificationManager.notify(notificationId, builder.build())
     }
 
 }
